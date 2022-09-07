7165bd113ad485fe31d2ca1f097de27d635f0d62
damianVarela <damian@skedgo.com>
2022-09-07 19:16:50 -0300

    add global schema validator.

--- src/test/java/com/buzzhives/regions/RegionsApplicationTests.java
+++ src/test/java/com/buzzhives/regions/RegionsApplicationTests.java
@@ -3,16 +3,21 @@ package com.buzzhives.regions;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.networknt.schema.JsonSchemaFactory;
 import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+import lombok.Cleanup;
 import lombok.extern.apachecommons.CommonsLog;
 import lombok.val;
 import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @CommonsLog
 class RegionsApplicationTests {
@@ -35,7 +40,38 @@ class RegionsApplicationTests {
         val errors = jsonSchema.validate(jsonNode);
         Assertions.assertThat(errors).isEmpty();
         log.info(" no errors found. ");
+    }
 
+    private Set<ValidationMessage> validRegion(@NotNull Path path) throws IOException {
+        val factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
+        val jsonSchema = factory.getSchema(Files.newInputStream(Paths.get("schemas/region.schema.json")));
+        val jsonNode = new ObjectMapper().readTree(path.toFile());
+        return jsonSchema.validate(jsonNode);
+    }
+
+    private Set<ValidationMessage> validPublicTransportFeed(@NotNull Path path) throws IOException {
+        val factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
+        val jsonSchema = factory.getSchema(Files.newInputStream(Paths.get("schemas/public-transport-feed.schema.json")));
+        val jsonNode = new ObjectMapper().readTree(path.toFile());
+        return jsonSchema.validate(jsonNode);
+    }
+
+    @Test
+    void valid() throws IOException {
+        @Cleanup val regions = Files.list(Paths.get("regions"));
+
+        for (val path : regions.collect(Collectors.toSet())) {
+            log.info("Validating " + path.toString());
+            val errors = validRegion(path);
+            Assertions.assertThat(errors).isEmpty();
+        }
+
+        @Cleanup val publicTransportFeeds = Files.list(Paths.get("publictransportfeeds"));
+        for (val path : publicTransportFeeds.collect(Collectors.toSet())) {
+            log.info("Validating " + path.toString());
+            val errors = validPublicTransportFeed(path);
+            Assertions.assertThat(errors).isEmpty();
+        }
     }
 
 }
