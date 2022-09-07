47a5fe845289d68d6056d8b2b6d585badca570c5
damianVarela <damian@skedgo.com>
2022-09-07 16:08:13 -0300

    fix text

--- src/test/java/com/buzzhives/regions/RegionsApplicationTests.java
+++ src/test/java/com/buzzhives/regions/RegionsApplicationTests.java
@@ -8,6 +8,7 @@ import lombok.val;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,34 +19,9 @@ class RegionsApplicationTests {
 
     @Test
     void validJson() throws IOException {
-        val factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4);
+        val factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
         val jsonSchema = factory.getSchema(Files.newInputStream(Paths.get("schemas/region.schema.json")));
-        val jsonNode = new ObjectMapper().readTree("{\n" +
-                "\t\"code\": \"ar_b_bahiablanca\",\n" +
-                "\t\"timezone\": \"America/Argentina/Buenos_Aires\",\n" +
-                "\t\"locale\": {\n" +
-                "\t\t\"language\": \"es\",\n" +
-                "\t\t\"country\": \"ar\"\n" +
-                "\t},\n" +
-                "\t\"currency\": {\n" +
-                "\t\t\"code\": \"ars\",\n" +
-                "\t\t\"symbol\": \"$\",\n" +
-                "\t\t\"format\": {\n" +
-                "\t\t\t\"unit\": \"$0\",\n" +
-                "\t\t\t\"subunit\": \"$0.00\"\n" +
-                "\t\t}\n" +
-                "\t},\n" +
-                "\t\"coverage\": {\n" +
-                "\t\t\"defaultLocation\": {\n" +
-                "\t\t\t\"lat\": 0,\n" +
-                "\t\t\t\"lng\": 0,\n" +
-                "\t\t\t\"address\": \"somewhere\"\n" +
-                "\t\t},\n" +
-                "\t\t\"polygon\": \" lat longs here \"\n" +
-                "\t},\n" +
-                "\t\"servers\": [ \"GRANDUNI\" ],\n" +
-                "\t\"feeds\": [\"SAPEM\"]\n" +
-                "}");
+        val jsonNode = new ObjectMapper().readTree(new File("regions/ar_b_bahiablanca.json"));
         val errors = jsonSchema.validate(jsonNode);
         Assertions.assertThat(errors).isEmpty();
         log.info(" no errors found. ");
