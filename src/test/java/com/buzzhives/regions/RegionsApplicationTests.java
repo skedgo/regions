dfae95853f01d18206bb5220d27cc8045843f40a
damianVarela <damian@skedgo.com>
2022-09-08 15:02:37 -0300

    more checks.

--- src/test/java/com/buzzhives/regions/RegionsApplicationTests.java
+++ src/test/java/com/buzzhives/regions/RegionsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.buzzhives.regions;
 
+import com.buzzhives.model.Code;
 import com.buzzhives.model.PublicTransportFeedSchema;
 import com.buzzhives.model.RegionSchema;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,7 +11,9 @@ import com.networknt.schema.ValidationMessage;
 import lombok.Cleanup;
 import lombok.extern.apachecommons.CommonsLog;
 import lombok.val;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Condition;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +22,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.ZoneId;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -29,6 +33,12 @@ class RegionsApplicationTests {
     private static final JsonSchema REGION_SCHEMA;
     @NotNull
     private static final JsonSchema PUBLIC_TRANSPORT_FEED_SCHEMA;
+
+
+    private static final Set<String> ISO_LANGUAGES = Arrays.stream(Locale.getISOLanguages()).map(String::toLowerCase).collect(Collectors.toSet());
+    private static final Set<String> ISO_COUNTRIES = Arrays.stream(Locale.getISOCountries()).map(String::toLowerCase).collect(Collectors.toSet());
+    private static final Set<String> ISO_CURRENCIES = Currency.getAvailableCurrencies().stream().map(Currency::getCurrencyCode).collect(Collectors.toSet());
+
 
     static {
         val factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
@@ -76,19 +86,37 @@ class RegionsApplicationTests {
     }
 
     @Test
-    void valid() throws IOException {
+    void validate() throws IOException {
         val regions = validateAndParse(RegionSchema.class, REGION_SCHEMA, "regions");
-        val publicTransportFeedsMap = validateAndParse(PublicTransportFeedSchema.class, PUBLIC_TRANSPORT_FEED_SCHEMA, "publictransportfeeds")
-                .stream()
-                .collect(Collectors.toMap(PublicTransportFeedSchema::getId, publicTransportFeed -> publicTransportFeed, (a, b) -> b, HashMap::new));
+        val publicTransportFeeds = validateAndParse(PublicTransportFeedSchema.class, PUBLIC_TRANSPORT_FEED_SCHEMA, "publictransportfeeds");
 
-        //check that all regions have a valid and existent feed
+        val publicTransportFeedsMap = new HashMap<String, PublicTransportFeedSchema>();
+        for (val publicTransportFeed : publicTransportFeeds) {
+            Assertions.assertThat(publicTransportFeed.getId()).isNotIn(publicTransportFeedsMap.keySet());
+            publicTransportFeedsMap.put(publicTransportFeed.getId(), publicTransportFeed);
+            Assertions.assertThat(publicTransportFeed.getSource().getUrl()).is(new Condition<>(s -> new UrlValidator().isValid(s), "a valid URL"));
+
+            //todo check that real time feeds are valid and existent
+        }
+
+
+        val regionCodes = new HashSet<Code>();
         for (val region : regions) {
+            //check the code of the regions and its uniqueness
+            val code = region.getCode();
+            Assertions.assertThat(code).isNotIn(regionCodes);
+            regionCodes.add(code);
+            Assertions.assertThat(code.getCountryCode()).isIn(ISO_COUNTRIES);
+            val locale = region.getLocale();
+            Assertions.assertThat(locale.getCountry()).isIn(ISO_COUNTRIES);
+            Assertions.assertThat(locale.getLanguage()).isIn(ISO_LANGUAGES);
+            Assertions.assertThat(region.getCurrency()).isIn(ISO_CURRENCIES);
+            Assertions.assertThat(region.getTimezone()).isIn(ZoneId.getAvailableZoneIds());
+
+            //check that all regions have a valid and existent feed
             val publicTransportFeedRefs = region.getFeeds();
-            if (publicTransportFeedRefs != null && !publicTransportFeedRefs.isEmpty())  {
-                val feedIds = publicTransportFeedRefs.toArray(new String[0]);
-                Assertions.assertThat(publicTransportFeedsMap).containsKeys(feedIds);
-            }
+            if (publicTransportFeedRefs != null && !publicTransportFeedRefs.isEmpty())
+                Assertions.assertThat(publicTransportFeedsMap).containsKeys(publicTransportFeedRefs.toArray(new String[0]));
         }
     }
 
